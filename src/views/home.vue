283ade108d57ff9b57570f9364b53074835073a1
MuthannaKhogali <2457764@dundee.ac.uk>
2025-01-08 15:32:58 +0000

    text field

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,6 +1,11 @@
 <template>
     <div class="container mt-5 text-center">
       <h1>QUESTION GENERATION PROTOTYPE</h1>
+    <div class="mb-3">
+    <form>
+      <input type="youtubeLink" class="form-control" id="exampleLink" placeholder="Enter youtube link">
+    </form>
+  </div>
     </div>
   </template>
   
