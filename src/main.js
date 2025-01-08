41646c2dff7d2cf265cbecea74ca8cb1712525e7
MuthannaKhogali <2457764@dundee.ac.uk>
2025-01-08 14:37:16 +0000

    set up web, bootstrap and router

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
-import './assets/main.css'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router/index.js';
 
-import { createApp } from 'vue'
-import App from './App.vue'
+// Import Bootstrap CSS and JS correctly for Vue 3
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(router);
+app.mount('#app');
---
+++ src/views/home.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container mt-5 text-center">
+      <h1>QUESTION GENERATION PROTOTYPE</h1>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'HomeView',
+  };
+  </script>
+  
+  <style scoped>
+  </style>
+  
