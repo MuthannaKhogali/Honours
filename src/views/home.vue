8b5342c80402b4321c9ff0c36b15478b08b9e760
MuthannaKhogali <2457764@dundee.ac.uk>
2025-01-08 15:35:17 +0000

    added generate button

--- src/views/home.vue
+++ src/views/home.vue
@@ -4,6 +4,7 @@
     <div class="mb-3">
     <form>
       <input type="youtubeLink" class="form-control" id="exampleLink" placeholder="Enter youtube link">
+      <button type="button" class="btn btn-success mt-3">Generate</button>
     </form>
   </div>
     </div>
