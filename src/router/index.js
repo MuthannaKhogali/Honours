1692c835d9860ac1ea7f11982f5ca245be7fd387
MuthannaKhogali <2457764@dundee.ac.uk>
2025-02-04 13:22:17 +0000

    cant just press back into accounts now

--- src/router/index.js
+++ src/router/index.js
@@ -21,13 +21,26 @@ const routes = [
   { 
     path: '/account', 
     name: 'account', 
-    component: AccountView
+    component: AccountView,
+    meta: { requiresAuth: true }
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(),
   routes
+});
+
+// protects routes
+router.beforeEach((to, from, next) => {
+  const isLoggedIn = !!localStorage.getItem('userID'); // check if userID exists in localStorage
+
+  if (to.meta.requiresAuth && !isLoggedIn) {
+    // redirect to login if trying to access a protected route while not logged in
+    next({ name: 'login' });
+  } else {
+    next(); // allow navigation
+  }
 });
 
 export default router;
